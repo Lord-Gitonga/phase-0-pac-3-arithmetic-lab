5fceff5a5ab405efe35c2384ed10f1d106d4986a
lordish <gitonga403@gmail.com>
2023-03-13 09:31:45 +0300

    Done Assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a,b){
+    return a+b;
+}
+console.log(add(4,3));
+
+function subtract(a,b){
+    return a-b;
+}
+console.log(subtract(4,3));
+
+function multiply(a,b){
+    return a*b;
+}
+console.log(4*3);
+
+function divide(a,b){
+    return a/b;
+}
+console.log(4/3);
+
+
+function increment(n){
+    n++;
+    return n;
+}
+  console.log(1);
+
+  function decrement(n){
+    n--;
+    return n;
+  }
+  console.log(1);
+
+  function makeInt(n){
+    (parseInt(n,10))
+    return(parseInt(n,10));
+}
+
+function preserveDecimal(n){
+    (parseFloat(n,10))
+    return(parseFloat(n,10));
+}
